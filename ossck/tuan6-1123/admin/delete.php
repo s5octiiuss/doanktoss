bf8702685cc6f95df118c7079c81b1e8686b02a4
quan <s5octiius@gmail.com>
2021-12-29 13:55:56 +0700

    done

--- ossck/tuan6-1123/admin/delete.php
+++ ossck/tuan6-1123/admin/delete.php
@@ -1,6 +1,6 @@
 <?php
 require_once './config.php';
 $id = $_GET['id'];
-$sql = "DELETE FROM book where id_book = '$id'";
+$sql = "DELETE FROM book where book_id = '$id'";
 $query = mysqli_query($connect, $sql);
 header("location: ../admin/admin.php");
