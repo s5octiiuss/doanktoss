e87814708a2e839206ae56778af2232ec94c93cc
NhatTan <skillstun159@gmail.com>
2021-12-29 12:35:08 +0700

    chucnangxemchitiet

---
+++ ossck/tuan6-1123/views/sach/detail.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../../../tuan6-1123/views/sach/index.css" type="text/css">
+
+<div class="cover-product-detail">
+    <p class="title"><?php echo $data['book_name'] ?></p>
+    <div class="cover-img-content">
+        <img class="product-img" src="../../../tuan6-1123/assets/img/book/<?php echo $data['img'] ?>" alt="Sách 1" width="200px" height="200px">
+        <p class="description"><?php echo $data['description'] ?></p>
+    </div>
+    <p class="price"><?php echo $data['price'] ?> vnd</p>
+</div>
