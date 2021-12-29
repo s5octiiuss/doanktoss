af871e46b49e9db02283b7face100bf2e7c2b6f5
NhatTan <skillstun159@gmail.com>
2021-12-29 13:12:39 +0700

    fixloihienthihinhanh

--- ossck/tuan6-1123/views/sach/detail.php
+++ ossck/tuan6-1123/views/sach/detail.php
@@ -3,7 +3,7 @@
 <div class="cover-product-detail">
     <p class="title"><?php echo $data['book_name'] ?></p>
     <div class="cover-img-content">
-        <img class="product-img" src="../../../tuan6-1123/assets/img/book/<?php echo $data['img'] ?>" alt="Sách 1" width="200px" height="200px">
+        <img class="product-img" src="../../../../doanktoss/ossck/tuan6-1123/assets/img/book/<?php echo $data['img'] ?>" alt="Sách 1" width="200px" height="200px">
         <p class="description"><?php echo $data['description'] ?></p>
     </div>
     <p class="price"><?php echo $data['price'] ?> vnd</p>
